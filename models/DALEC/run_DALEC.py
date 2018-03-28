759212420cdbcd662f74e29368efea40ff408f2a
Cosmin Safta <csafta@sandia.gov>
2018-03-28 01:51:53 -0700

    adding capability to read samples from file. backwards compatible - it should run the same as before if parameter samples are not provided.

--- models/DALEC/run_DALEC.py
+++ models/DALEC/run_DALEC.py
@@ -7,13 +7,31 @@ import argparse
 
 from utils import *
 
+# default parameters
+nens = 2 # ensemble size
+
 parser = argparse.ArgumentParser()
 
-# main parameters
-parser.add_argument("-s","--site", dest="site", default="US-UMB", type=str, help="Site ID")
+# command-line arguments
+parser.add_argument("-s","--site",   dest="site",    default="US-UMB", type=str, help="Site ID")
+parser.add_argument("-p","--parspl", dest="parfile", default=None,     type=str, help="Parameter Samples")
 options = parser.parse_args()
 
 site = options.site
+spls = numpy.array([])
+if options.parfile is not None:
+    infile = open(options.parfile, 'r')
+    firstLine = infile.readline()
+    infile.close()
+    splsNames=firstLine.split()
+    spls = numpy.genfromtxt(options.parfile,skip_header=1)
+    spls = numpy.atleast_2d(spls) 
+    print(spls.shape)
+    nens  = spls.shape[0] # ensemble size
+    npars = spls.shape[1] # no. of parameters
+    if (len(splsNames)!=npars):
+        print('Parameter file %s is not consistent'%(options.parfile))
+
 print('Processing site: %s'%(site))
 
 #create model object
@@ -36,30 +54,47 @@ numpy.savetxt('gpp_obs.txt',model.obs['gpp'])
 model.plot_output(startyear=2000,endyear=2005)
 
 #---------------------- run an ensemble ----------------------------
-
-nens = 2 #50     #ensemble size
 p_all   = numpy.zeros([nens,model.nparms],numpy.float)
 gpp_all = numpy.zeros([nens,12],numpy.float)  #Monthly GPP (mean seasonal cycle over all years)
 nee_all = numpy.zeros([nens,12],numpy.float)  #Monthly NEE (mean seasonal cycle over all years)
 lai_all = numpy.zeros([nens,12],numpy.float)  #Monthly LAI (mean seasonal cycle over all years)
 
+# some more checks
+if spls.shape[0]>0:
+    if npars!=model.nparms:
+        print('Not enough parameters in the samples file')
+        quit()
+
 names_out = open('pnames.txt','w')
+if spls.shape[0]>0:
+    for p in splsNames:
+        names_out.write(p+'\n')
+else:
+    for p in model.parms:
+        names_out.write(p+'\n')
+
+names_out.close()
+
 for i in range(0,nens):
     print 'Running #'+str(i+1)
-    pnum=0
-    for p in model.parms:
-        #Sample uniformly from the parameter space
-        model.parms[p] = numpy.random.uniform(low=model.pmin[p],high=model.pmax[p])
-        #Save the parameters
-        p_all[i,pnum] = model.parms[p]
-        if (i == 0):
-            names_out.write(p+'\n')
-        pnum=pnum+1
+    if spls.shape[0]>0:
+        # Sample in [-1,1] from file
+        for k in range(model.nparms):
+            p = splsNames[k]
+            model.parms[p]=model.pmin[p]+0.5*(spls[i,k]+1)*(model.pmax[p]-model.pmin[p])
+            p_all[i,k] = model.parms[p]
+    else:
+        pnum=0
+        for p in model.parms:
+            #Sample uniformly from the parameter space
+            model.parms[p] = numpy.random.uniform(low=model.pmin[p],high=model.pmax[p])
+            #Save the parameters
+            p_all[i,pnum] = model.parms[p]
+            pnum=pnum+1
     model.run(spinup_cycles=6,deciduous=True)
     gpp_all[i,:] = daily_to_monthly(model.output['gpp'],allyearsmean=True)
     nee_all[i,:] = daily_to_monthly(model.output['nee'],allyearsmean=True)
     lai_all[i,:] = daily_to_monthly(model.output['lai'],allyearsmean=True)
-names_out.close()
 
 #Save "UQ-ready" outputs
 numpy.savetxt('p_ensemble.txt',p_all)
