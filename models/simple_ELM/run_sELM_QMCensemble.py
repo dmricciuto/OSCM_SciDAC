837b526c4d837cafa91d5f931aa81afa775c2718
Daniel <ricciutodm@ornl.gov>
2018-07-26 10:09:44 -0700

    Add runscript for QMC ensemble

--- models/simple_ELM/run_sELM_QMCensemble.py
+++ models/simple_ELM/run_sELM_QMCensemble.py
@@ -11,25 +11,28 @@ from utils import *
 model = models.MyModel()
 
 #site level data
-site = 'US-UMB'
+#site = 'US-UMB'
 #load site forcings
-model.load_forcings(site=site)
+#model.load_forcings(site=site)
 #Load site observations
 #model.load_obs(site)
 
 #---------------------- run an ensemble ----------------------------
 
-pnames_ens = ['leafcn','slatop_decid','q10_mr','frootcn','froot_leaf','br_mr','crit_dayl','gdd_crit']
-n_ensemble = 5000
-myoutvars = ['gpp','lai','npp','totvegc']
-print 'Generating ensemble'
-model.generate_ensemble(n_ensemble, pnames_ens, fname='QMCsample.dat')
-#splsNames=[]
-#for v in model.parms:
-#    splsNames.append(v)
+#pnames_ens = ['leafcn','slatop_decid','q10_mr','frootcn','froot_leaf','br_mr','crit_dayl','gdd_crit']
 
+#generate an ensemble with all parameters
+pnames_ens=[]
+for v in model.parms:
+    pnames_ens.append(v)
+
+n_ensemble = 2000
+myoutvars = ['gpp','lai']
+print 'Generating ensemble'
+
+#model.generate_ensemble(n_ensemble, pnames_ens, fname='QMCsample2.dat')
 model.generate_ensemble(n_ensemble, pnames_ens)
 
-model.run_selm(spinup_cycles=6, pft=0, do_monthly_output=True, use_MPI=True)
-#model.run_selm(spinup_cycles=4, lon_bounds=[-96,-66], lat_bounds=[28,48], pft=0, do_monthly_output=True,  myoutvars=myoutvars, use_MPI=True)
+#model.run_selm(spinup_cycles=6, pft=0, do_monthly_output=True, use_MPI=True)
+model.run_selm(spinup_cycles=4, lon_bounds=[-96,-66], lat_bounds=[28,48], pft=0, do_monthly_output=True,  myoutvars=myoutvars, use_MPI=True)
 
